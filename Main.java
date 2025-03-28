59c3448b2a61fbd495d7f35aba777856a6acdde5
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2025-03-28 17:25:16 +0000

    skibidify the rizzler please

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ public class Main
   public static void main(String[] args)
   {
     // TODO: Create objects and test code here.
+    SpecialtyCoffee n1 = new SpecialtyCoffee();
+    SpecialtyCoffee n2 = new SpecialtyCoffee("large", "mocha", "caramel");
+    SpecialtyCoffee n3 = new SpecialtyCoffee("medium", true, 2, "cappuccino", "gingerbread");
+    System.out.println(n1.toString());
+    System.out.println(n2.toString());
+    System.out.println(n3.toString());
   }
 }
---
+++ SpecialtyCoffee.java
@@ -0,0 +1,19 @@
+public class SpecialtyCoffee extends Coffee{
+    private String flavor;
+    public SpecialtyCoffee(){
+        super("small", false, 1, "latte");
+        this.flavor = "vanilla";
+    }
+    public SpecialtyCoffee(String size, String type, String flavor){
+        super(size, false, 1, type);
+        this.flavor = flavor;
+    }
+    public SpecialtyCoffee(String size, boolean isSkinny, int shots, String type, String flavor){
+        super(size, isSkinny, shots, type);
+        this.flavor = flavor;
+    }
+  public String toString()
+  {
+    return super.toString() + " with " + flavor;
+  }
+}
